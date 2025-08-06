b484b5c81de64f3edc81bc12920b8a2240386b76
Wilfredo Porta <wporta@gmail.com>
2025-08-05 23:18:50 -0600

    setting up prisma and adding basic endpoints

--- src/features/publisher/publisher.controller.ts
+++ src/features/publisher/publisher.controller.ts
@@ -1,8 +1,45 @@
 import { Request, Response } from 'express';
 
-const getAllPublishers = (req: Request, res: Response) => {
-  //return 'Get All Publishers from Controller';
-  res.status(200).end('Get All Publishers from Controller');
+import {
+  getPublishers,
+  getPublisherById,
+  createPublisher,
+} from './publisher.service';
+
+export const getAll = async (req: Request, res: Response) => {
+  const publishers = await getPublishers();
+
+  if (publishers) {
+    res.status(200).json(publishers);
+  } else {
+    res.status(404).end('Publishers not found');
+  }
 };
 
-export { getAllPublishers };
+export const getById = async (req: Request, res: Response) => {
+  const publisherId = Number.parseInt(req.params.id);
+
+  if (publisherId) {
+    const publisher = await getPublisherById(publisherId);
+
+    if (publisher) {
+      res.status(200).json(publisher);
+    } else {
+      res
+        .status(404)
+        .json({ message: `Publisher with id: ${publisherId} not found.` });
+    }
+  } else {
+    res.status(404).json({ message: 'Id is required' });
+  }
+
+  console.log(publisherId);
+};
+
+export const create = async (req: Request, res: Response) => {
+  const publisherData = req.body;
+
+  const created = await createPublisher(publisherData);
+
+  res.status(201).json(created);
+};
